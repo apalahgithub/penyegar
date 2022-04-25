ee60afa4cd5a45cf9556ea20b0c7a8f4be21cd9a
apalahgithub <104360359+apalahgithub@users.noreply.github.com>
2022-04-25 19:37:32 +0700

    Create button.py

---
+++ plugins/button.py
@@ -0,0 +1,176 @@
+# Credits: @ANDXINN
+
+from config import FORCE_SUB_CHANNEL, FORCE_SUB_GROUP
+from pyrogram.types import InlineKeyboardButton
+
+
+def start_button(client):
+    if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Tentang Bot", callback_data="about"),
+                InlineKeyboardButton(text=" Tutup", callback_data="close"),
+            ],
+        ] 
+     return buttons
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink4),
+            ],
+            [
+                InlineKeyboardButton(text="Tentang Saya", callback_data="about"),
+                InlineKeyboardButton(text="Tutup", callback_data="close"),
+            ],
+        ]
+     return buttons
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink3),
+            ],
+            [
+                InlineKeyboardButton(text="Tentang Saya", callback_data="about"),
+                InlineKeyboardButton(text="Tutup", callback_data="close"),
+            ],
+        ]
+     return buttons
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
+            ],
+            [
+                InlineKeyboardButton(text="Tentang Saya", callback_data="about"),
+                InlineKeyboardButton(text="Tutup", callback_data="close"),
+            ],
+        ]
+        return buttons
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink),
+            ],
+            [
+                InlineKeyboardButton(text="Tentang Saya", callback_data="about"),
+                InlineKeyboardButton(text="Tutup", callback_data="close"),
+            ],
+        ]
+     return buttons
+    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Tentang Saya", callback_data="about"),
+            ],
+            [
+                InlineKeyboardButton(text="Join Channel I", url=client.invitelink),
+                InlineKeyboardButton(text="Join Channel II", url=client.invitelink2),
+                InlineKeyboardButton(text="Join Channel III", url=client.invitelink3),
+                InlineKeyboardButton(text="Join Channel IV", url=client.invitelink4),
+            ],
+            [InlineKeyboardButton(text="Tutup", callback_data="close")],
+        ]
+        return buttons
+        
+        
+   def fsub_button(client, message):
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink4),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+        def fsub_button(client, message):
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink3),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+        def fsub_button(client, message):
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+        def fsub_button(client, message):
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+        if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel I", url=client.invitelink),
+                InlineKeyboardButton(text="Join Channel II", url=client.invitelink2),
+                InlineKeyboardButton(text="Join Channel II", url=client.invitelink3),
+                InlineKeyboardButton(text="Join Channel IV", url=client.invitelink4),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+
+       
+       
+        
